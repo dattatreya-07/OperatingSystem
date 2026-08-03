0b8b01e86404f649a6a010be9c1a4b5a24be68e6
Dattatreya M K <mkannankannanmkannan_bcs28@mepcoeng.ac.in>
2026-08-03 11:06:08 +0530

    Add IPC example using pipes in msg.c
    Implement a simple IPC using pipes and fork in C.

---
+++ 2.Implementation_Of_IPC/msg.c
@@ -0,0 +1,35 @@
+#include<stdio.h>
+#include<unistd.h>
+#include<sys/wait.h>
+int main(){
+   int fd[2];
+   pid_t pid;
+   char write_msg[]="kello";
+   char read_msg[100];
+   if(pipe(fd)==-1){
+      perror("pipe creation failed");
+      return 1;
+   }
+   pid=fork();
+   if(pid<0){
+      perror("fork failed");
+      return 1;
+   }
+   if(pid==0){
+      close(fd[1]);
+      int bytes_read=read(fd[0],read_msg,sizeof(read_msg)-1);
+      if(bytes_read>0){
+	 read_msg[bytes_read]='\o';
+	 printf("child received:%s\n",read_msg);
+      }
+      close(fd[0]);
+   }
+   else{
+      close(fd[0]);
+      printf("Parent sending:%s\n",write_msg);
+      write(fd[1],write_msg,strlen(write_msg)+1);
+      close(fd[1]);
+      wait(NULL);
+   }
+   return 0;
+}
